1997842f0d7da29b6603831df2e994b46a3561dc
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-09-22 06:17:04 +0800

    fixing the prod bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import ContextProvider from "./context";
 import { headers } from "next/headers";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "@/store/Providers";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "C6Credits",
@@ -21,31 +20,6 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="C6Credits is a decentralized carbon credit system. It allows you to earn credits by reducing your carbon footprint."
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <title>C6Credits</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-        />
-        <link ref="favicon" href="/favicon.ico" />
-      </Head>
       <body>
         <ContextProvider cookies={cookies}>
           <Providers>{children}</Providers>
