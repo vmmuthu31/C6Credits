acc8013f19859ac2bfbbf82fad522524613c7c06
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-09-22 03:53:34 +0800

    updated the connect btn login state

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -11,8 +11,14 @@ import {
   optimism,
   polygon,
 } from "@reown/appkit/networks";
-import React, { type ReactNode } from "react";
-import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+import React, { useState, type ReactNode } from "react";
+import {
+  cookieToInitialState,
+  useAccount,
+  useDisconnect,
+  WagmiProvider,
+  type Config,
+} from "wagmi";
 
 const queryClient = new QueryClient();
 
@@ -47,13 +53,55 @@ export function CustomButton({ props }: { props: string }) {
 }
 
 export function ConnectButton() {
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const shortenAddress = (address: string) =>
+    `${address.slice(0, 4)}...${address.slice(-4)}`;
+
+  const getButtonStyles = () => {
+    if (address) {
+      return "bg-green-600 text-white hover:bg-green-700";
+    } else {
+      return "bg-[#F6F4EB] text-[#002A16] hover:bg-[#d4d2c7]";
+    }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <button
-      className="bg-[#F6F4EB] text-[#002A16] font-medium text-lg px-4 py-2 rounded-full"
-      onClick={() => modal.open()}
-    >
-      Connect Wallet
-    </button>
+    <div className="relative inline-block">
+      <button
+        className={`font-medium text-lg px-6 py-3 rounded-full transition duration-300 transform hover:scale-105 ${getButtonStyles()}`}
+        onClick={address ? toggleDropdown : () => modal.open()}
+      >
+        {address ? shortenAddress(address) : "Connect Wallet"}
+      </button>
+
+      {isDropdownOpen && (
+        <div
+          className="absolute right-0 mt-2 w-48 bg-red-500  shadow-xl rounded-lg z-10
+                      transition ease-out duration-200 transform scale-100"
+        >
+          <ul className="py-1">
+            <li>
+              <button
+                className="block w-full text-left px-4 py-2   rounded-t-lg transition duration-150 ease-in-out"
+                onClick={() => {
+                  disconnect();
+                  setIsDropdownOpen(false);
+                }}
+              >
+                Disconnect
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
