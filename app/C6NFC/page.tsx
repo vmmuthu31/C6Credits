66ec4b9344b5c0740d78e59fe5a31462df0d63b7
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-09-22 08:11:10 +0800

    updated the c6nfc

--- app/C6NFC/page.tsx
+++ app/C6NFC/page.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+"use client";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import axios from "axios";
 import { Spin, Button, Modal } from "antd";
